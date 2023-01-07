9f587733fbf2db2e9fea91e87f8e8677c3271686
Ekocahyonoputro <108456288+Ekocahyonoputro@users.noreply.github.com>
2023-01-08 06:51:20 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,24 +27,24 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
+global.botname = "☠Eko༒Botz☠" //namabot kalian
+global.ownername= "MAS XOE" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/@ChZeeoneOfc" //bebas asal jan hapus
+global.youtube = "https://youtube.com/@MAS XOE" //bebas asal jan hapus
 global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.email = "jagoanterang19@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326","622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
-global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.donasi = "./image/donasi.jpg" // foto donasi di folder image
+global.premium = ['628122168003'] //premium user
+global.owner = ["628122168003","622150996855"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "628122168003" // nomor wa kalian
+global.ownernomerr = "+628122168003" //nmr wa kalian
+global.thumbnail = "./y/lol.jpeg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.donasi = "./k/donasi.jpeg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© ☠Eko༒Botz☠' //sticker wm ubah
+global.author = 'Di Buat Oleh ☠Eko༒Botz☠' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
